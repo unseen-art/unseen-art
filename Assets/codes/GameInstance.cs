b79ac3824b8f3cc51a6cee356370c42d4fd4d829
aiyou4love <aiyou4love@outlook.com>
2017-07-06 13:21:38 +0800

    u

---
+++ Assets/codes/GameInstance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : MonoBehaviour {
+
+	public static GameInstance Instance {
+		get {
+			if (null == instance) {
+				instance = FindObjectOfType<GameInstance> ();
+			}
+			return instance;
+		}
+	}
+
+	static GameInstance instance;
+
+	void Awake() {
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
