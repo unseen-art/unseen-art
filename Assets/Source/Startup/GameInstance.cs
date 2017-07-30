549b3dee85b6bd4a9b9e411cde91dcf71d28cc0f
unseen-art <unseen-art@outlook.com>
2017-07-30 10:48:24 +0800

    u

---
+++ Assets/Source/Startup/GameInstance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : MonoBehaviour
+{
+	void Awake()
+	{
+		mInstance = FindObjectOfType<GameInstance> ();
+	}
+
+	void Start ()
+	{
+		DontDestroyOnLoad (this);
+
+		StartupClient startup = new StartupClient ();
+		startup.RunInit ();
+		startup.RunStart ();
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public static GameInstance Instance
+	{
+		get 
+		{
+			return mInstance;
+		}
+	}
+
+	static GameInstance mInstance;
+}
